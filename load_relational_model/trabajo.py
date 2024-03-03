af5195570e2200d98048827e42e45ece146cff7a
Nicolas Valderrama <nvalderrama@truora.com>
2024-03-03 14:53:44 -0500

    Add: Trabajo script

---
+++ load_relational_model/trabajo.py
@@ -0,0 +1,42 @@
+import csv
+from datetime import datetime
+import os
+from loadBD import logger
+
+
+def load_data(conexion):
+    try:
+        logger.info("Starting job data loading process")
+
+        # Verificar si la conexión fue exitosa
+        if conexion.is_connected():
+            logger.info("Conexión exitosa a la base de datos MySQL")
+            # Crear cursor
+            cursor = conexion.cursor()
+
+            # Nombre del archivo CSV
+            archivo_csv = os.path.join("..", "trabajo.csv")
+
+            # Consulta para insertar datos en la tabla
+            consulta = "INSERT INTO trabajo (idTrabajo,idEstudiante,fechaInicio,fechaFin,compania,pais,sector,salario) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+
+            # Leer datos del archivo CSV e insertarlos en la base de datos
+            with open(archivo_csv, newline="", encoding="utf-8") as csvfile:
+                lector_csv = csv.DictReader(csvfile)
+                for fila in lector_csv:
+                    # Suponiendo que tus columnas en la tabla se llaman igual que las columnas en el CSV
+                    datos = (fila['idTrabajo'], fila['idEstudiante'], datetime.strptime(fila['fechaInicio'], "%m/%d/%Y").strftime("%Y-%m-%d"), datetime.strptime(
+                        fila['fechaFin'], "%m/%d/%Y").strftime("%Y-%m-%d"), fila['compania'], fila['pais'], fila['sector'], fila['salario'])  # Ajusta esto según la estructura de tu tabla
+                    cursor.execute(consulta, datos)
+
+            # Confirmar la transacción
+            conexion.commit()
+            logger.info("Datos insertados correctamente.")
+
+            # Cerrar cursor y conexión
+            cursor.close()
+            conexion.close()
+        else:
+            logger.error("No se pudo conectar a la base de datos MySQL")
+    except Exception as e:
+        logger.error(f"Error during job data loading: {e}")
